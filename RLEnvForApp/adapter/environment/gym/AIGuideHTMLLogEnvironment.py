47ec6d451fed981bddc33e916939a539e94fd719
Tim <ligi.info73@yahoo.com>
2024-03-10 20:28:27 +0800

    fix: Make 'AIGuideHTMLLogEnvironment.py' to snake case

--- RLEnvForApp/adapter/environment/gym/AIGuideHTMLLogEnvironment.py
+++ RLEnvForApp/adapter/environment/gym/AIGuideHTMLLogEnvironment.py
@@ -23,104 +23,104 @@ class AIGuideHTMLLogEnvironment(gym.Env):
     def __init__(self, episodeHandlerRepository: EpisodeHandlerRepository = Provide[EnvironmentDIContainers.episodeHandlerRepository]):
         self._logger = Logger()
         self._logger.info('Init environment.Env')
-        self._applicationIp = "127.0.0.1"
-        self._applicationPort = 3100
+        self._application_ip = "127.0.0.1"
+        self._application_port = 3100
 
-        self._episodeHandlerRepository = episodeHandlerRepository
+        self._episode_handler_repository = episodeHandlerRepository
 
         self._crawler = HTMLLogCrawler()
-        self._autOperator = AIGUIDEOperator(crawler=self._crawler)
+        self._aut_operator = AIGUIDEOperator(crawler=self._crawler)
 
-        self._targetPagePort = TargetPagePortFactory().createAIGuideHTMLLogTargetPagePort(
+        self._target_page_port = TargetPagePortFactory().createAIGuideHTMLLogTargetPagePort(
             folderPath="htmlSet/GUIDE_HTML_SET")
 
-        self._targetPageId = ""
-        self._episodeHandlerId = ""
-        self._episodeIndex = 0
-        self._stepNumber = 1
-        self._totalStep = 1
-        self._episodeReward = 0
-        self._stepsInformation = ""
-        self._originalObservation = {}
+        self._target_page_id = ""
+        self._episode_handler_id = ""
+        self._episode_index = 0
+        self._step_number = 1
+        self._total_step = 1
+        self._episode_reward = 0
+        self._steps_information = ""
+        self._original_observation = {}
 
-        initiateEnvUseCase = InitiateEnvironmentUseCase.InitiateEnvironmentUseCase()
-        initiateEnvInput = InitiateEnvironmentInput.InitiateEnvironmentInput()
-        initiateEnvOutput = InitiateEnvironmentOutput.InitiateEnvironmentOutput()
-        initiateEnvUseCase.execute(input=initiateEnvInput, output=initiateEnvOutput)
+        initiate_env_use_case = InitiateEnvironmentUseCase.InitiateEnvironmentUseCase()
+        initiate_env_input = InitiateEnvironmentInput.InitiateEnvironmentInput()
+        initiate_env_output = InitiateEnvironmentOutput.InitiateEnvironmentOutput()
+        initiate_env_use_case.execute(input=initiate_env_input, output=initiate_env_output)
 
-        self._observation_shape = initiateEnvOutput.getObservationSize()
+        self._observation_shape = initiate_env_output.getObservationSize()
 
-        self.action_space = gym.spaces.Discrete(initiateEnvOutput.getActionSpaceSize())
+        self.action_space = gym.spaces.Discrete(initiate_env_output.getActionSpaceSize())
         self.observation_space = gym.spaces.Box(low=-numpy.inf,
                                                 high=numpy.inf,
                                                 shape=self._observation_shape,
                                                 dtype=numpy.float32)
 
-        self._targetPagePort.connect()
-        self._targetPagePort.waitForTargetPage()
+        self._target_page_port.connect()
+        self._target_page_port.waitForTargetPage()
 
     def step(self, action):
-        if self._autOperator.getFocusedAppElement() is None:
-            focusElementXpath = ""
+        if self._aut_operator.getFocusedAppElement() is None:
+            focus_element_xpath = ""
         else:
-            focusElementXpath = self._autOperator.getFocusedAppElement().getXpath()
+            focus_element_xpath = self._aut_operator.getFocusedAppElement().getXpath()
 
-        executeActionUseCase = ExecuteActionUseCase.ExecuteActionUseCase(
-            autOperator=self._autOperator)
-        executeActionInput = ExecuteActionInput.ExecuteActionInput(actionNumber=int(action),
-                                                                   episodeHandlerId=self._episodeHandlerId)
-        executeActionOutput = ExecuteActionOutput.ExecuteActionOutput()
-        executeActionUseCase.execute(input=executeActionInput, output=executeActionOutput)
+        execute_action_use_case = ExecuteActionUseCase.ExecuteActionUseCase(
+            autOperator=self._aut_operator)
+        execute_action_input = ExecuteActionInput.ExecuteActionInput(actionNumber=int(action),
+                                                                     episodeHandlerId=self._episode_handler_id)
+        execute_action_output = ExecuteActionOutput.ExecuteActionOutput()
+        execute_action_use_case.execute(input=execute_action_input, output=execute_action_output)
 
-        self._stepsInformation = "total_step:" + f"{self._totalStep:4}" + \
-                                 "\tStep:" + f"{self._stepNumber:2}" + \
-                                 "\tEpisode:" + str(self._episodeIndex) + \
+        self._steps_information = "total_step:" + f"{self._total_step:4}" + \
+                                 "\tStep:" + f"{self._step_number:2}" + \
+                                 "\tEpisode:" + str(self._episode_index) + \
                                  "\tAction:" + f"{int(action):2}" + \
-                                 "\tReward:" + f"{executeActionOutput.getReward(): 4.3f}" + \
-                                 "\tFocusElement: " + str(self._originalObservation).ljust(64) + \
-                                 "\tXpath: " + focusElementXpath + \
-                                 "\tCodeCoverage:" + str(executeActionOutput.getCodeCoverageDict())
-        observation = numpy.array(executeActionOutput.getObservation())
+                                 "\tReward:" + f"{execute_action_output.getReward(): 4.3f}" + \
+                                 "\tFocusElement: " + str(self._original_observation).ljust(64) + \
+                                 "\tXpath: " + focus_element_xpath + \
+                                  "\tCodeCoverage:" + str(execute_action_output.getCodeCoverageDict())
+        observation = numpy.array(execute_action_output.getObservation())
         observation.resize(self._observation_shape)
-        self._episodeReward += executeActionOutput.getReward()
-        self._stepNumber += 1
-        self._totalStep += 1
+        self._episode_reward += execute_action_output.getReward()
+        self._step_number += 1
+        self._total_step += 1
 
-        self._logger.info(self._stepsInformation)
+        self._logger.info(self._steps_information)
 
-        self._originalObservation = executeActionOutput.getOriginalObservation()
-        self._originalObservation = self._stripDictionaryContents(self._originalObservation)
-        return observation, executeActionOutput.getReward(), executeActionOutput.getIsDone(), {
-            "Reward": executeActionOutput.getReward()}
+        self._original_observation = execute_action_output.getOriginalObservation()
+        self._original_observation = self._stripDictionaryContents(self._original_observation)
+        return observation, execute_action_output.getReward(), execute_action_output.getIsDone(), {
+            "Reward": execute_action_output.getReward()}
 
     def reset(self):
-        self._logger.info("Episode reward:" + f"{self._episodeReward:3}")
-        self._stepsInformation = ""
-        self._episodeReward = 0
+        self._logger.info("Episode reward:" + f"{self._episode_reward:3}")
+        self._steps_information = ""
+        self._episode_reward = 0
 
         self._logger.info("\n\n=======================Reset environment.Env=======================")
-        self._episodeIndex += 1
-        self._stepNumber = 1
+        self._episode_index += 1
+        self._step_number = 1
 
-        resetEnvUseCase = ResetEnvironmentUseCase.ResetEnvironmentUseCase(
-            operator=self._autOperator)
-        resetEnvUseInput = ResetEnvironmentInput.ResetEnvironmentInput(
-            episodeIndex=self._episodeIndex)
-        resetEnvUseOutput = ResetEnvironmentOutput.ResetEnvironmentOutput()
-        resetEnvUseCase.execute(input=resetEnvUseInput, output=resetEnvUseOutput)
+        reset_env_use_case = ResetEnvironmentUseCase.ResetEnvironmentUseCase(
+            operator=self._aut_operator)
+        reset_env_use_input = ResetEnvironmentInput.ResetEnvironmentInput(
+            episodeIndex=self._episode_index)
+        reset_env_use_output = ResetEnvironmentOutput.ResetEnvironmentOutput()
+        reset_env_use_case.execute(input=reset_env_use_input, output=reset_env_use_output)
 
         self._logger.info("Episode Handler Amount:" +
-                          f"{len(self._episodeHandlerRepository.findAll()):2}")
-        self._logger.info("Target page url is: " + resetEnvUseOutput.getTargetPageUrl())
+                          f"{len(self._episode_handler_repository.findAll()):2}")
+        self._logger.info("Target page url is: " + reset_env_use_output.getTargetPageUrl())
         self._logger.info("==========================================================\n\n")
 
-        self._episodeHandlerId = resetEnvUseOutput.getEpisodeHandlerId()
-        self._targetPageId = resetEnvUseOutput.getTargetPageId()
-        observation = numpy.array(resetEnvUseOutput.getObservation())
+        self._episode_handler_id = reset_env_use_output.getEpisodeHandlerId()
+        self._target_page_id = reset_env_use_output.getTargetPageId()
+        observation = numpy.array(reset_env_use_output.getObservation())
         observation.resize(self._observation_shape)
 
-        self._originalObservation = resetEnvUseOutput.getOriginalObservation()
-        self._originalObservation = self._stripDictionaryContents(self._originalObservation)
+        self._original_observation = reset_env_use_output.getOriginalObservation()
+        self._original_observation = self._stripDictionaryContents(self._original_observation)
         return observation
 
     def close(self):
