f4f7c29d769a51be0450ff93e29e6f641fe86e18
Tim <ligi.info73@yahoo.com>
2024-03-31 21:43:55 +0800

    fixup: temp

--- RLEnvForApp/adapter/agent/model/builder/builder.py
+++ RLEnvForApp/adapter/agent/model/builder/builder.py
@@ -1,1 +1,34 @@
-class builder:
+from abc import abstractmethod, ABC
+
+
+class Builder(ABC):
+    """
+    Interface for model builders.
+    """
+    @abstractmethod
+    def reset(self):
+        """
+        Reset the builder.
+        :return:
+        """
+
+    @abstractmethod
+    def set_template(self):
+        """
+        Set the template.
+        :return: None
+        """
+
+    @abstractmethod
+    def set_llm_model(self, model_name: str, model_path: str):
+        """
+        Set the LLM model.
+        :return: None
+        """
+
+    @abstractmethod
+    def set_verbalizer(self):
+        """
+        Set the verbalizer.
+        :return: None
+        """
