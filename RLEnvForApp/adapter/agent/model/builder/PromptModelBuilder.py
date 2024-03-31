e5b1c69dd1a26b81194c97ecb30c0628489062bf
Tim <ligi.info73@yahoo.com>
2024-03-31 21:43:55 +0800

    fixup: temp

--- RLEnvForApp/adapter/agent/model/builder/PromptModelBuilder.py
+++ RLEnvForApp/adapter/agent/model/builder/PromptModelBuilder.py
@@ -1,3 +1,46 @@
+from openprompt.plms import load_plm
+from openprompt.prompts import ManualTemplate, PrefixTuningTemplate
+from RLEnvForApp.adapter.agent.model.builder.Builder import Builder
 
-class PromptModelBuilder:
-    pass
+
+class PromptModelBuilder(Builder):
+    """
+    Builder for PromptModel of OpenPrompt.
+    """
+
+    def __init__(self):
+        self.template = None
+        self.plm = None
+        self.tokenizer = None
+        self.model_config = None
+        self.WrapperClass = None
+
+    def reset(self):
+        if self.__result is not None:
+            self.__result = None
+
+    def set_template(self, template_type: str, template_text: str):
+        """
+        Set the template. Before setting the template, the LLM model should be set.
+        :param template_type:
+        :param template_text:
+        :return:
+        """
+        if template_type == "manual":
+            self.template = ManualTemplate(tokenizer=self.tokenizer, text=template_text)
+        elif template_type == "prefix":
+            self.template = PrefixTuningTemplate(model=self.plm, tokenizer=self.tokenizer)
+        else:
+            raise NotImplementedError
+
+    def set_llm_model(self, model_name: str, model_path: str):
+        """
+        Set the LLM model.
+        :param model_name:
+        :param model_path:
+        :return:
+        """
+        self.plm, self.tokenizer, self.model_config, self.WrapperClass = load_plm(model_name, model_path)
+
+    def set_verbalizer(self):
+        pass
