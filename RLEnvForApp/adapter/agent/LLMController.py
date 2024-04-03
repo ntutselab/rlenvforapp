dbed22176c6ad85e9215c941a7b8caf8784d5d6f
Tim <ligi.info73@yahoo.com>
2024-04-03 10:16:33 +0800

    fixup: temp

--- RLEnvForApp/adapter/agent/LLMController.py
+++ RLEnvForApp/adapter/agent/LLMController.py
@@ -1,3 +1,12 @@
+import torch
+from RLEnvForApp.adapter.agent.model.builder.PromptModelBuilder import PromptModelBuilder
+from RLEnvForApp.adapter.agent.model.builder.PromptModelDirector import PromptModelDirector
+
 
 class LLMController:
-    pass
+    def play(self):
+        prompt_model_builder = PromptModelBuilder()
+        prompt_model = PromptModelDirector().make_my_research(prompt_model_builder)
+        # check cuda
+        if torch.cuda.is_available():
+            prompt_model = prompt_model.cuda()
--- RLEnvForApp/adapter/agent/model/builder/PromptModelBuilder.py
+++ RLEnvForApp/adapter/agent/model/builder/PromptModelBuilder.py
@@ -1,46 +1,119 @@
+import torch
+from openprompt import PromptForClassification
 from openprompt.plms import load_plm
-from openprompt.prompts import ManualTemplate, PrefixTuningTemplate
+from openprompt.prompts import ManualTemplate, PrefixTuningTemplate, SoftTemplate, ManualVerbalizer
 from RLEnvForApp.adapter.agent.model.builder.Builder import Builder
 
 
 class PromptModelBuilder(Builder):
     """
     Builder for PromptModel of OpenPrompt.
+    1. set llm model
+    2. set template
+    3. set verbalizer
+    4. set state dict
     """
 
     def __init__(self):
+        self.state_dict_path = None
+        self.__result = None
+        self.verbalizer = None
         self.template = None
         self.plm = None
         self.tokenizer = None
         self.model_config = None
-        self.WrapperClass = None
+        self.wrapper_class = None
 
     def reset(self):
+        """
+        Reset the builder.
+        :return: None
+        """
         if self.__result is not None:
             self.__result = None
+
+    def get_result(self):
+        """
+        Get the result of the builder.
+        :return: PromptModel
+        """
+        if self.__check_parameters():
+            self.__result = PromptForClassification(
+                template=self.template,
+                plm=self.plm,
+                verbalizer=self.verbalizer,
+                freeze_plm=True,
+                plm_eval_mode=False
+            )
+            # TODO: Cann't lock into pipfile.lock when I install torch. May need to install torch manually.
+            self.__result.load_state_dict(torch.load(self.state_dict_path))
+        return self.__result
 
     def set_template(self, template_type: str, template_text: str):
         """
         Set the template. Before setting the template, the LLM model should be set.
-        :param template_type:
-        :param template_text:
+        :param template_type: Type of template.
+        :param template_text: Text of the template.
         :return:
         """
+        # TODO: Add more template types.
         if template_type == "manual":
             self.template = ManualTemplate(tokenizer=self.tokenizer, text=template_text)
         elif template_type == "prefix":
             self.template = PrefixTuningTemplate(model=self.plm, tokenizer=self.tokenizer)
+        elif template_type == "soft":
+            self.template = SoftTemplate(model=self.plm, tokenizer=self.tokenizer, text=template_text)
         else:
             raise NotImplementedError
 
     def set_llm_model(self, model_name: str, model_path: str):
         """
         Set the LLM model.
-        :param model_name:
-        :param model_path:
+        :param model_name: Name of the model.
+        :param model_path: Path to the model.
         :return:
         """
-        self.plm, self.tokenizer, self.model_config, self.WrapperClass = load_plm(model_name, model_path)
+        self.plm, self.tokenizer, self.model_config, self.wrapper_class = (
+            load_plm(model_name, model_path))
 
-    def set_verbalizer(self):
-        pass
+    def set_verbalizer(self, verbalizer_type:str, labels_list: list, labels_word_file_path: str):
+        """
+        Set the verbalizer.
+        Load the predefined label words from verbalizer file.
+        Currently, support three types of file format:
+        1. a .jsonl or .json file, in which is a single verbalizer
+        in dict format.
+        2. a .jsonal or .json file, in which is a list of verbalizers in dict format
+        3. a .txt or a .csv file, in which is the label words of a class are listed in line,
+        separated by commas. Begin a new verbalizer by an empty line.
+        This format is recommended when you don't know the name of each class.
+        :param verbalizer_type: Type of verbalizer.
+        :param labels_list: List of class labels.
+        :param labels_word_file_path: Path to the file containing the class labels.
+        :return:
+        """
+        if verbalizer_type == "manual":
+            self.verbalizer = (ManualVerbalizer(tokenizer=self.tokenizer, classes=labels_list)
+                               .from_file(labels_word_file_path))
+        else:
+            raise NotImplementedError
+
+    def set_state_dict(self, state_dict_path: str):
+        """
+        Set the state_dict of the model.
+        :param state_dict_path: Path to the state_dict.
+        :return:
+        """
+        self.state_dict_path = state_dict_path
+
+    def __check_parameters(self):
+        if self.template is None:
+            raise ValueError("Template is not set.")
+        elif self.verbalizer is None:
+            raise ValueError("Verbalizer is not set.")
+        elif self.plm is None:
+            raise ValueError("LLM model is not set.")
+        elif self.state_dict_path is None:
+            raise ValueError("State dict is not set.")
+        else:
+            return True
